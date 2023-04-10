ba09ff0dd5aceec4a04f0c63a24b5ba7fce10bb3
ummagohil <umgo0510@gmail.com>
2023-04-10 21:21:16 +0100

    dasiy ui + dropdowns

--- components/MealType.tsx
+++ components/MealType.tsx
@@ -1,3 +1,20 @@
-const MealType = () => <div>Meal Type</div>;
+const MealType = () => (
+  <div className="dropdown dropdown-hover">
+    <label tabIndex={0} className="btn bg-emerald-400 m-1">
+      Meal Type
+    </label>
+    <ul
+      tabIndex={0}
+      className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+    >
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <a>Item 2</a>
+      </li>
+    </ul>
+  </div>
+);
 
 export default MealType;
