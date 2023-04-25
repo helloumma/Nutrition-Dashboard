18793ab101b4e32a7065c611ebbb91fa4aceb0c6
ummagohil <umgo0510@gmail.com>
2023-04-25 16:56:56 +0100

    current code

--- pages/data/getData.ts
+++ pages/data/getData.ts
@@ -3,25 +3,35 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useFetchData = (value: string) => {
   const fetchData = async () => {
-    const response = await fetch(
-      `https://trackapi.nutritionix.com/v2/search/instant?query=${value}&common=true&branded=true`,
-      {
-        headers: {
-          "x-app-id": `${process.env.ID}`,
-          "x-app-key": `${process.env.API_KEY}`,
-        },
+    try {
+      const response = await fetch(
+        `https://trackapi.nutritionix.com/v2/search/instant?query=${value}&common=true`,
+        {
+          headers: {
+            "x-app-id": `${process.env.ID}`,
+            "x-app-key": `${process.env.API_KEY}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-    );
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+      const data = await response.json();
+      return data.common;
+    } catch (error) {
+      console.log(error);
+      // display an error message to the user or retry the query
+      throw error;
     }
-
-    const data = await response.json();
-    return data.common;
   };
 
-  return useQuery<getData[]>(["foods", value], fetchData);
+  return useQuery<getData[]>(["foods", value], fetchData, {
+    onError: (error) => {
+      console.log(error);
+    },
+  });
 };
 
 /*const fetchData = async (): Promise<getData> => {
